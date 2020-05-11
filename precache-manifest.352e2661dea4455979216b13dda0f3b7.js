69e857f559cac1a6247f99af3ba17c1f3e8c0427
Travis <travisnevins@outlook.com>
2020-05-10 20:53:41 -0400

    Updates

---
+++ precache-manifest.352e2661dea4455979216b13dda0f3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c8d006a3480df5512b715368c8b458",
+    "url": "/react-data-table/index.html"
+  },
+  {
+    "revision": "cfcb8485247e22feefd0",
+    "url": "/react-data-table/static/css/main.d40054ee.chunk.css"
+  },
+  {
+    "revision": "cc7151fa8b8f57e969ca",
+    "url": "/react-data-table/static/js/2.56baa0f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-data-table/static/js/2.56baa0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcb8485247e22feefd0",
+    "url": "/react-data-table/static/js/main.c74d5a63.chunk.js"
+  },
+  {
+    "revision": "d01d736169bdd583e7e7",
+    "url": "/react-data-table/static/js/runtime-main.e80f1938.js"
+  }
+]);
